2813c74012cc10208c43183b3230504c8c3aad03
Silver_Hsiao <silver041692@gmail.com>
2022-06-12 04:37:50 +0800

    期末作業:新增header、JS隨機挑圖、Discord聊天室嵌入

---
+++ demo/Final_Project/js/random_image.js
@@ -0,0 +1,6 @@
+path = "./media/"
+const p = document.querySelector('.BGImg')
+i = Math.floor(Math.random() * 28+1);
+var url = (path + i + ".png");
+
+p.innerHTML = "<img src="+url+" width='1000' height='1000'>"
