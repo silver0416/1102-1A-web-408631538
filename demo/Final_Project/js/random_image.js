3e37c418bb0fa5b1109f16dcb4863c1ef40c27e8
Silver_Hsiao <silver041692@gmail.com>
2022-06-12 17:53:52 +0800

    add footer

--- demo/Final_Project/js/random_image.js
+++ demo/Final_Project/js/random_image.js
@@ -1,6 +1,5 @@
 path = "./media/"
 const p = document.querySelector('.BGImg')
-i = Math.floor(Math.random() * 28+1);
+i = Math.floor(Math.random() * 28 + 1);
 var url = (path + i + ".png");
-
-p.innerHTML = "<img src="+url+" width='1000' height='1000'>"
+p.innerHTML = "<img src=" + url + " width='1000' height='1000'>"
