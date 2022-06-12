746f7b7fad667387ed7147da15c36740bf4b9b01
Silver_Hsiao <silver041692@gmail.com>
2022-06-12 23:45:30 +0800

    新增header手機板選單

--- demo/Final_Project/js/random_image.js
+++ demo/Final_Project/js/random_image.js
@@ -2,4 +2,4 @@ path = "./media/"
 const p = document.querySelector('.BGImg')
 i = Math.floor(Math.random() * 28 + 1);
 var url = (path + i + ".png");
-p.innerHTML = "<img src=" + url + " width='1000' height='1000'>"
+p.innerHTML = "<img src=" + url + " width='1000' height='1500'>"
