b3a3ed49067b16e350b4b732898570e2d1937b36
Silver_Hsiao <silver041692@gmail.com>
2022-06-15 19:22:11 +0800

    完成hero頁面隨機背景、調整hero頁面

--- demo/Final_Project/js/random_image.js
+++ demo/Final_Project/js/random_image.js
@@ -1,5 +1,9 @@
-path = "./media/"
-const p = document.querySelector('.hero-BGImg')
-i = Math.floor(Math.random() * 28 + 1);
-var url = (path + i + ".png");
-p.innerHTML = "<img src=" + url + " class='hero-BGImg-img'>"
+function putRandomBGImage() {
+    path = "./media/"
+    const p = document.querySelector('.hero-BGImg')
+    i = Math.floor(Math.random() * 28 + 1);
+    var url = (path + i + ".png");
+    document.body.style.backgroundImage = "url("+url+")";
+    document.body.style.backgroundAttachment = 'fixed';
+    document.body.style.backgroundSize = '100% 100%';
+}
