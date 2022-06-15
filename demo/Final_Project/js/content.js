0617588edf8a2c2d0c2625e0ff5b53ae6cb42657
Silver_Hsiao <silver041692@gmail.com>
2022-06-15 18:12:24 +0800

    修正版面改為section並新增文字

--- demo/Final_Project/js/content.js
+++ demo/Final_Project/js/content.js
@@ -1,14 +1,14 @@
-function change(index) {
-    const p = document.querySelector('.show-content')
-    switch (index) {
-        case 0:
-            p.innerHTML = `<iframe src="./resume.html" width="100%" height="100%"/>`
-            break
-        case 1:
-            p.innerHTML = `<iframe src="./BOTPu.html" width="100%" height="100%"/>`
-            break
-        case 2:
-            p.innerHTML = `<iframe src="./gallery.html" width="100%" height="100%"/>`
-            break
-    }
-}
+// function change(index) {
+//     const p = document.querySelector('.show-content')
+//     switch (index) {
+//         case 0:
+//             p.innerHTML = `<iframe src="./resume.html" width="100%" height="100%"/>`
+//             break
+//         case 1:
+//             p.innerHTML = `<iframe src="./BOTPu.html" width="100%" height="100%"/>`
+//             break
+//         case 2:
+//             p.innerHTML = `<iframe src="./gallery.html" width="100%" height="100%"/>`
+//             break
+//     }
+// }
--- demo/Final_Project/js/random_image.js
+++ demo/Final_Project/js/random_image.js
@@ -1,5 +1,5 @@
 path = "./media/"
-const p = document.querySelector('.BGImg')
+const p = document.querySelector('.hero-BGImg')
 i = Math.floor(Math.random() * 28 + 1);
 var url = (path + i + ".png");
-p.innerHTML = "<img src=" + url + " width='1000' height='1500'>"
+p.innerHTML = "<img src=" + url + " class='hero-BGImg-img'>"
